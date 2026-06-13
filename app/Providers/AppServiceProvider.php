a10155d7095127bcdf1e1590c900c849a3da5c30
VitorGabrielAquino <vitor.aquino.oak@gmail.com>
2026-06-13 15:31:31 -0300

    feat: configure passport oauth foundation

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Passport\Passport;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Passport::tokensCan([
+            'payments:read' => 'Read payment requests',
+            'payments:create' => 'Create payment requests',
+            'payments:approve' => 'Approve or reject payment requests',
+        ]);
     }
 }
--- config/auth.php
+++ config/auth.php
@@ -35,11 +35,16 @@ return [
     |
     | Supported: "session"
     |
-    */
+     */
 
     'guards' => [
         'web' => [
             'driver' => 'session',
+            'provider' => 'users',
+        ],
+
+        'api' => [
+            'driver' => 'passport',
             'provider' => 'users',
         ],
     ],
